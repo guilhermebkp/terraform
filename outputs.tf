a3c7cce312f9d9559e5a8f3dae3e2fc8c10f2eed
guilhermebkp <guilherm774@gmail.com>
2025-11-10 13:14:22 -0300

    Update outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,57 @@
+
+# ------------------------
+# output Bloco CIDR
+# ------------------------
+output "aws_vpc" {
+value = aws_vpc.vpc_model.cidr_block
+description = "Bloco CIDR da VPC"      
+}
+
+# ------------------------
+# output Bloco Subnet Publica
+# ------------------------
+output "aws_vpc" {
+value = aws_subnet.public_subnet_pub.cidr_block
+description = "Bloco CIRD Subnet Publica"
+}
+
+# ------------------------
+# output Bloco Subnet App
+# ------------------------
+output "aws_vpc" {
+value = aws_subnet.private_subnet_app.cidr_block
+description = "Bloco CIRD Subnet App"
+}
+
+# ------------------------
+# output Bloco Subnet Data
+# ------------------------
+output "aws_vpc" {
+value = aws_subnet.private_subnet_data.cidr_block
+description = "Bloco CIRD Subnet Data"
+}
+
+
+# ------------------------
+# output Route Table Public
+# ------------------------
+output "aws_vpc" {
+value = aws_route_table.rtb_public.route
+description = "Blocos associados a tabela de public"
+}
+
+# ------------------------
+# output Route Table App
+# ------------------------
+output "aws_vpc" {
+value = aws_route_table.rtb_app.route
+description = "Blocos associados a tabela de rota app"
+}
+
+# ------------------------
+# output Route Table Data
+# ------------------------
+output "aws_vpc" {
+value = aws_route_table.rtb_data.route
+description = "Blocos associados a tabela de rota data"
+}
